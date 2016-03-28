17669a01ae4c6ad32ce01b114fb24964d6db65f5
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-03-28 14:36:40 -0700

    Issue #2693151 by jhedstrom: Location of CKEditor Templates library is hardcoded

--- src/Plugin/CKEditorPlugin/Templates.php
+++ src/Plugin/CKEditorPlugin/Templates.php
@@ -7,8 +7,12 @@
 namespace Drupal\wysiwyg_template\Plugin\CKEditorPlugin;
 
 use Drupal\ckeditor\CKEditorPluginInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\editor\Entity\Editor;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Defines the CKEditor Templates plugin.
@@ -22,7 +26,54 @@ use Drupal\editor\Entity\Editor;
  *   module = "wysiwyg_template"
  * )
  */
-class Templates extends PluginBase implements CKEditorPluginInterface {
+class Templates extends PluginBase implements CKEditorPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * The wysiwyg_template.settings config object.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * The current request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * Constructs the CKEditor Templates plugin.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The current request stack.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, RequestStack $request_stack) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->config = $config_factory->get('wysiwyg_template.settings');
+    $this->requestStack = $request_stack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+      $container->get('request_stack')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -51,7 +102,7 @@ class Templates extends PluginBase implements CKEditorPluginInterface {
   public function getFile() {
     // @todo This location is hard-coded and should be more flexible.
     // @see https://www.drupal.org/node/2693151
-    return \Drupal::request()->getBaseUrl() . '/libraries/templates/plugin.js';
+    return $this->requestStack->getCurrentRequest()->getBaseUrl() . '/' . $this->config->get('library_path') . '/plugin.js';
   }
 
   /**
