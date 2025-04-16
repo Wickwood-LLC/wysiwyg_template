101d320144adf3138f175164a24f37ede922241f
Junaid PV <junu.pv@gmail.com>
2025-04-16 20:10:18 +0530

    Use of "static" in callables is deprecated

--- src/Entity/Template.php
+++ src/Entity/Template.php
@@ -175,7 +175,7 @@ class Template extends ConfigEntityBase implements TemplateInterface {
     parent::postLoad($storage, $entities);
     // Sort the queried roles by their weight.
     // See \Drupal\Core\Config\Entity\ConfigEntityBase::sort().
-    uasort($entities, 'static::sort');
+    uasort($entities, static::sort(...));
   }
 
   /**
